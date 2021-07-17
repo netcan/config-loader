09f15c0710b9eeeae30d050e24dfd7116cf569a7
netcan <1469709759@qq.com>
2021-07-17 21:57:33 +0800

    add ut/CMakeLists.txt

--- third-party/ThirdParty.cmake
+++ third-party/ThirdParty.cmake
@@ -1,13 +1,19 @@
 add_subdirectory(third-party/Catch2)
 
 if (HAS_TINYXML2)
+add_compile_definitions(HAS_TINYXML2)
 add_subdirectory(third-party/tinyxml2)
+list(APPEND THIRD_PARTY_LIBRARIES tinyxml2::tinyxml2)
 endif()
 
 if (HAS_JSONCPP)
+add_compile_definitions(HAS_JSONCPP)
 add_subdirectory(third-party/jsoncpp)
+list(APPEND THIRD_PARTY_LIBRARIES jsoncpp_static)
 endif()
 
 if (HAS_YAMLCPP)
+add_compile_definitions(HAS_YAMLCPP)
 add_subdirectory(third-party/yaml-cpp)
+list(APPEND THIRD_PARTY_LIBRARIES yaml-cpp)
 endif()

f9580433f8f3a8c7fbb4a487f0ee0e8241a54195
netcan <netcan1996@gmail.com>
2025-12-04 22:32:06 +0800

    docs: add IP statement assigning rights to Huawei

--- include/config-loader/serialize/SerializeTraits.h
+++ include/config-loader/serialize/SerializeTraits.h
@@ -1,4 +1,11 @@
 //
+// This project is a personal hobby work. The author hereby assigns all
+// intellectual property rights in this project to Huawei Technologies Co., Ltd.,
+// and imposes no obligations or liabilities on Huawei.
+//
+// 本项目为作者个人业余创作，作者在此声明将本项目及其衍生作品相关的全部知识产权无偿
+// 转让给华为技术有限公司，且不对华为提出任何义务或责任要求。
+//
 // Created by netcan on 2021/08/06.
 //
 
